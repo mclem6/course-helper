1716237b1145dc9fb81e6c3ee881d4c5523c026f
Maria Clement <147575008+mclem6@users.noreply.github.com>
2025-04-22 20:54:12 -0500

    updated access screen

---
+++ coursehelperapp/src/main/java/com/coursehelper/AccessScreenController.java
@@ -0,0 +1,35 @@
+package com.coursehelper;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class AccessScreenController {
+
+    @FXML
+    private void login() throws IOException {
+        //authenticate user 
+
+        //if user exists// correct info entered, load user's home page 
+
+        //if not alert user of error
+    }
+
+    @FXML
+    private void signup() throws IOException {
+        //open signup options
+        
+        //load create account button
+
+    
+    }
+
+    private void createaccount() throws IOException{
+        //verify is information entered correctly
+
+        //if verified create account
+
+        //if not alert user of error
+    }
+
+
+}
