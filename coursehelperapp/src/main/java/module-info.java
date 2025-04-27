22f4979f730bf0f6c3b6826c8c186b366b57ada8
mclem6 <mclem6@uic.edu>
2025-04-27 10:20:17 -0500

    accesspage progress

--- coursehelperapp/src/main/java/module-info.java
+++ coursehelperapp/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.coursehelper {
     requires javafx.controls;
     requires javafx.fxml;
+    requires transitive javafx.graphics;
 
     opens com.coursehelper to javafx.fxml;
     exports com.coursehelper;
