e96974632ddd61054900be6024766772320a4771
mclem6 <mclem6@uic.edu>
2025-05-29 19:40:18 -0500

    added homepage sections, merging and deleting branch

--- coursehelperapp/src/main/java/com/coursehelper/controllers/HomePageController.java
+++ coursehelperapp/src/main/java/com/coursehelper/controllers/HomePageController.java
@@ -8,7 +8,7 @@ public class HomePageController {
     @FXML
     Text welcomeText;
 
-    public void welcomeText(String userName){
+    public void setWelcomeMessage(String userName){
         welcomeText.setText("Welcome, " + userName);
     }
 
